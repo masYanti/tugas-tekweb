9b21f373e1531ac0535e7bf2063867deccb7d95d
masYanti <hafidzshandyyahya@gmail.com>
2024-11-04 19:53:59 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('hamburger-button').addEventListener('click', function() {
+    var mobileMenu = document.getElementById('mobile-menu');
+    mobileMenu.classList.toggle('hidden');
+  });
